a0c41e8867975296b55b486da54a9120be49d8f4
Aston <Aston@LAPTOP-JSKCS3J4>
2022-12-14 16:31:52 +0100

    afegit codi de provaa

---
+++ pruebam5/src/pruebam5/Pruebam5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package pruebam5;
+
+import java.util.Scanner;
+
+
+public class Pruebam5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hola");
+    }
+
+}
